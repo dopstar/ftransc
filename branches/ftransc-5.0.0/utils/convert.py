150aa286a8be72bf0e91e7209c05b4e71310aadc
mmadlavana@gmail.com <mmadlavana@gmail.com@abc20b46-1a99-9d3c-1a9b-2685fd156449>
2012-07-23 08:18:39 +0000

    ftransc 5.0.3
    git-svn-id: https://ftransc.googlecode.com/svn@57 abc20b46-1a99-9d3c-1a9b-2685fd156449

--- branches/ftransc-5.0.0/utils/convert.py
+++ branches/ftransc-5.0.0/utils/convert.py
@@ -5,7 +5,7 @@ def convert_to_mp3(filename, ext, stderr_out, outdir, preset=None):
     converts input track to MPEG-1 Layer III format (aka MP3)
     """
     outdir = outdir or './'
-    cmd1 = ["ffmpeg", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
+    cmd1 = ["avconv", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
     cmd2 = ("lame " + preset).split()
     cmd2.extend(["-", outdir + filename + ".mp3"])
     return convert(stderr_out, cmdline1=cmd1, cmdline2=cmd2)
@@ -15,7 +15,7 @@ def convert_to_m4a(filename, ext, stderr_out, outdir, preset=None):
     converts input track into MPEG-4 audio format (aka AAC/M4A/MP4)
     """
     outdir = outdir or './'
-    cmd1 = ["ffmpeg", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
+    cmd1 = ["avconv", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
     cmd2 = ("faac " + preset).split()
     cmd2.extend(["-o", outdir + filename + ".m4a", "/dev/stdin"])
     return convert(stderr_out, cmdline1=cmd1, cmdline2=cmd2)
@@ -25,7 +25,7 @@ def convert_to_wma(filename, ext, stderr_out, outdir, preset=None):
     converts an input track into the Microsoft ASF format (aka WMA)
     """
     outdir = outdir or './'
-    cmd1 = ["ffmpeg", "-y", "-i", filename + ext]
+    cmd1 = ["avconv", "-y", "-i", filename + ext]
     cmd2 =  preset.split()
     cmd1.extend(cmd2)
     cmd1.append(outdir + filename + ".wma")
@@ -36,7 +36,7 @@ def convert_to_ogg(filename, ext, stderr_out, outdir, preset=None):
     converts input track into Ogg Vorbis format
     """
     outdir = outdir or './'
-    cmd1 = ["ffmpeg", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
+    cmd1 = ["avconv", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
     cmd2 = ("oggenc " + preset).split()
     cmd2.extend(["-o", outdir + filename + ".ogg", "/dev/stdin"])
     return convert(stderr_out, cmdline1=cmd1, cmdline2=cmd2)
@@ -46,7 +46,7 @@ def convert_to_wav(filename, ext, stderr_out, outdir, preset=None):
     decodes a track into WAVE format. This format has no metadata support.
     """
     outdir = outdir or './'
-    cmd = ["ffmpeg", "-y", "-i", filename + ext, 
+    cmd = ["avconv", "-y", "-i", filename + ext,
             "-f", "wav", outdir + filename + ".wav"]
     return convert(stderr_out, cmdline1=cmd)
 
@@ -67,7 +67,7 @@ def convert_to_mpc(filename, ext, stderr_out, preset=''):
     converts input track into Musepack audio format
     """
     outdir = outdir or './'
-    cmd1 = ["ffmpeg", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
+    cmd1 = ["avconv", "-y", "-i", filename + ext, "-f", "wav", "/dev/stdout"]
     cmd2 = ("mppenc " + preset).split()
     cmd2.extend(["-", outdir + filename + ".mpc"])
     return convert(stderr_out, cmdline1=cmd1, cmdline2=cmd2)
