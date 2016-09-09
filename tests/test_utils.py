77e41251aaca76be8b25ff5efd5f8ab5072c80de
dopstar <mka@takealot.com>
2016-09-09 07:46:11 +0200

    completed tests for presets

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,36 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import os
-import time
 import json
-import logging
 import unittest
-import datetime
-from collections import deque
 
 import ftransc.utils as futils
 
 
 class UtilsTestCase(unittest.TestCase):
-    def test_get_audio_presets__mp3_normal_internal(self):
-        self.assertEqual(futils.get_audio_presets('mp3'), u'-vn -ac 2 -acodec libmp3lame -aq 4')
-        self.assertEqual(futils.get_audio_presets('mp3', 'normal'), u'-vn -ac 2 -acodec libmp3lame -aq 4')
+    @classmethod
+    def setUpClass(cls):
+        fixture_filename = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'presets.json'))
+        with open(fixture_filename) as fd:
+            cls.presets = json.load(fd)
 
-    def test_get_audio_preset__mp3_insane_internal(self):
-        self.assertEqual(futils.get_audio_presets('mp3', 'insane'), u'-vn -ac 2 -acodec libmp3lame -ab 320k')
-    
-    def test_get_audio_preset__mp3_extreme_internal(self):
-        self.assertEqual(futils.get_audio_presets('mp3', 'extreme'), u'-vn -ac 2 -acodec libmp3lame -aq 0')
-
-    def test_get_audio_preset__mp3_high_internal(self):
-        self.assertEqual(futils.get_audio_presets('mp3', 'high'), u'-vn -ac 2 -acodec libmp3lame -aq 2')
-
-    def test_get_audio_preset__mp3_low_internal(self):
-        self.assertEqual(futils.get_audio_presets('mp3', 'low'), u'-vn -ac 2 -acodec libmp3lame -aq 9')
-
-    def test_get_audio_preset__mp3_tiny_internal(self): 
-        self.assertEqual(futils.get_audio_presets('mp3', 'tiny'), u'-vn -ac 1 -acodec libmp3lame -ab 32k -ar 32000')
-
+    def test_presets(self):
+        for audio_format, preset in self.presets.iteritems():
+            is_external = audio_format.endswith('_ext')
+            audio_format = audio_format.split('_')[0]
+            for audio_quality, quality_preset in preset.iteritems():
+                self.assertEquals(futils.get_audio_presets(audio_format, audio_quality, is_external), quality_preset)
 
 if __name__ == '__main__':
     unittest.main()
