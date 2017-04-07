155eff0caef8b3d86870183022295491a925fe1d
dopstar <mka@takealot.com>
2017-04-07 22:51:03 +0200

    including travis build for py36

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup_args = {
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     'entry_points': {
         'console_scripts': [
